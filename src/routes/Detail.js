91796336e4b5fcf29ab245d079056191fc7b3abc
piw601625 <piw601625@gmail.com>
2022-10-16 02:30:23 +0900

    movie

---
+++ src/routes/Detail.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function Detail() {
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
+  const { id } = useParams();
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovie(json.data.movie);
+    console.log(movie);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <img src={movie.medium_cover_image} alt="movie" />
+          <div>{movie.title}</div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Detail;
